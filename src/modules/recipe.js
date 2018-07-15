f9f82b7c2a5b58e7d819b5d4f3d91966dcca3cc9
Souvik Chakraborty <souvik.c5@tcs.com>
2018-07-16 00:45:33 +0530

    changes in the reducers in the cart compoenent

--- src/modules/recipe.js
+++ src/modules/recipe.js
@@ -1,34 +1,45 @@
-
 // API imports
 
 // ------------------------------------
 // Constants
 // ------------------------------------
 
-export const ADD_RECIPE = 'ADD_RECIPE';
-export const ACTIVE_RECIPE = 'ACTIVE_RECIPE';
+export const ADD_RECIPE = "ADD_RECIPE";
+export const ACTIVE_RECIPE = "ACTIVE_RECIPE";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const DELETE_RECIPE = "DELETE_RECIPE";
 
 // ------------------------------------
-// Action Creators
+// Action
 // ------------------------------------
 
-export const addRecipe = (recipe) => ({
+export const addRecipe = recipe => ({
   type: ADD_RECIPE,
-  payload: recipe,
+  payload: recipe
 });
 
-export const addActiveRecipe = (recipe) => ({
-    type: ACTIVE_RECIPE,
-    payload: recipe
-})
+export const addActiveRecipe = recipe => ({
+  type: ACTIVE_RECIPE,
+  payload: recipe
+});
+
+export const addRecipeToCart = recipe => ({
+  type: ADD_TO_CART,
+  payload: recipe
+});
+
+export const deleteRecipe = recipes => ({
+  type: DELETE_RECIPE,
+  payload: recipes
+});
 
 // ------------------------------------
 // Action Creators ( For Asynchronous calls )
 // ------------------------------------
 
 // export const addRecipeToStore = (recipe) => (dispatch) => {
-//     dispatch(addRecipe(recipe));   
-// } 
+//     dispatch(addRecipe(recipe));
+// }
 
 // export const actions = {
 //     addRecipe,
@@ -41,12 +52,21 @@ export const addActiveRecipe = (recipe) => ({
 const ACTION_HANDLERS = {
   [ADD_RECIPE]: (state, action) => ({
     ...state,
-    recipes: [...state.recipes, action. payload],
+    recipes: [...state.recipes, action.payload]
   }),
   [ACTIVE_RECIPE]: (state, action) => ({
-      ...state,
-      activeRecipe: action.payload
-  })
+    ...state,
+    activeRecipe: action.payload
+  }),
+  [ADD_TO_CART]: (state, action) => ({
+    ...state,
+    cartRecipes: [...state.cartRecipes, action.payload]
+  }),
+  [DELETE_RECIPE]: (state, action) => ({
+    ...state,
+    cartRecipes: action.payload,
+    recipes: action.payload
+  }),
 };
 
 // ------------------------------------
@@ -55,7 +75,8 @@ const ACTION_HANDLERS = {
 
 const initialState = {
   recipes: [],
-  activeRecipe: 'No Active recipe'
+  cartRecipes: [],
+  activeRecipe: "No Active recipe"
 };
 
 export default (state = initialState, action) => {
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,15 +2,17 @@ import { connect } from "react-redux";
 import Cart from "./Cart.jsx";
 
 // action creators
-import { addRecipeToCart, deleteRecipe } from './../../modules/cart'
+// import { addRecipeToCart, deleteRecipe } from './../../modules/cart'
+import { addRecipeToCart, deleteRecipe } from "./../../modules/recipe";
 
-const mapDispatchToProps =  {
-    addRecipeToCart,
-    deleteRecipe
+const mapDispatchToProps = {
+  addRecipeToCart,
+  deleteRecipe
 };
 
 const mapStateToProps = state => ({
-    cartRecipes: state.cartReducer.cartRecipes
+  // cartRecipes: state.cartReducer.cartRecipes
+  cartRecipes: state.recipeReducer.cartRecipes
 });
 
 export default connect(
